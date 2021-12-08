0018d0216dda710e5fd63e70eb046e22fdeb0b65
Abhiram Laha <abhiramlaha2020@gmail.com>
2021-12-08 10:45:25 +0530

    master

--- String/S4.cpp
+++ String/S4.cpp
@@ -1,11 +1,29 @@
 /* C++ Program to Compare Two Strings */
 #include<iostream>
+#include<string.h>
 using namespace std;
  
 int main(){
     cout<<"-----Compare Two Strings-------\n\n";
 
+    string str1;
+    cout<<"Enter First String : ";
+    getline(cin,str1);
+
+    string str2;
+    cout<<"\nEnter Second String : ";
+    getline(cin,str2);
+ 
+    cout<<endl;
     
+    if (str1==str2){
+        cout<<"Given Strings are SAME.";
+    }
+    else{
+        cout<<"Given Strings are DIFFERENT.";
+    }
+
+
 
 return 0;
 }
